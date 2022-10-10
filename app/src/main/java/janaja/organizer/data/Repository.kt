15f062544f6f709493d7d98b59af307a5fd2f8f8
JanaJa <janajansen@live.de>
2022-10-11 01:12:04 +0200

    fix getNote function

--- app/src/main/java/janaja/organizer/data/Repository.kt
+++ app/src/main/java/janaja/organizer/data/Repository.kt
@@ -13,15 +13,18 @@ class Repository {
         Note(4, "Kaufen", mutableListOf("Spaghetti","Hände","Tomaten","brrrr","lelel","Spaghetti","Hände","Tomaten","brrrr","lelel","Tomaten","brrrr","lelel"))
     )
     val dummyTodoData = mutableListOf(
-        Note(0, "Täglich", mutableListOf("Wasser trinken", "1 Teil aufräumen", "Essen"),true),
-        Note(1, "Wöchentlich", mutableListOf("ICh mache nOtiz","Super toll"), true),
-        Note(2, "Monatlich", mutableListOf("GuNa","hehe","länger"), true),
-        Note(4, "Kaufen", mutableListOf("Spaghetti","Hände","Tomaten","brrrr","lelel","Spaghetti","Hände","Tomaten","brrrr","lelel","Tomaten","brrrr","lelel"), true)
+        Note(5, "Täglich", mutableListOf("Wasser trinken", "1 Teil aufräumen", "Essen"),true),
+        Note(6, "Wöchentlich", mutableListOf("ICh mache nOtiz","Super toll"), true),
+        Note(7, "Monatlich", mutableListOf("GuNa","hehe","länger"), true),
+        Note(8, "Kaufen", mutableListOf("Spaghetti","Hände","Tomaten","brrrr","lelel","Spaghetti","Hände","Tomaten","brrrr","lelel","Tomaten","brrrr","lelel"), true)
     )
 
     fun getNote(id: Long): Note? {
         // TODO dummy method
         dummyNoteData.filter { note: Note -> note.id == id }.also {
+            if (it.isNotEmpty()) return it[0]
+        }
+        dummyTodoData.filter { note: Note -> note.id == id }.also {
             return if (it.isNotEmpty()) it[0] else null
         }
     }
