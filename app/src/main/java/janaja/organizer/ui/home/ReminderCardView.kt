f7476e331a953dfe96d43f6f2dc50c588954f54e
JanaJa <janajansen@live.de>
2022-10-11 01:35:45 +0200

    change reminder_cardview_header.xml

--- app/src/main/java/janaja/organizer/ui/home/ReminderCardView.kt
+++ app/src/main/java/janaja/organizer/ui/home/ReminderCardView.kt
@@ -7,18 +7,17 @@ import androidx.cardview.widget.CardView
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import janaja.organizer.R
-import janaja.organizer.adapter.CategorySpinnerAdapter
 import janaja.organizer.adapter.ReminderRecyclerViewAdapter
 import janaja.organizer.data.Repository
 import janaja.organizer.databinding.HomeCardviewBinding
 import janaja.organizer.databinding.NoteCardviewContentBinding
-import janaja.organizer.databinding.NoteCardviewHeaderBinding
+import janaja.organizer.databinding.ReminderCardviewHeaderBinding
 
 class ReminderCardView(context: Context, attrs: AttributeSet) : CardView(context, attrs) {
 
     private val gurki = "beste babe"
     private val binding: HomeCardviewBinding
-    private val headerBinding: NoteCardviewHeaderBinding
+    private val headerBinding: ReminderCardviewHeaderBinding
     private val contentBinding: NoteCardviewContentBinding
 
     init {
@@ -32,16 +31,11 @@ class ReminderCardView(context: Context, attrs: AttributeSet) : CardView(context
         // manage header
         headerBinding = DataBindingUtil.inflate(
             LayoutInflater.from(context),
-            R.layout.note_cardview_header,
+            R.layout.reminder_cardview_header,
             binding.flHomeCardviewHeader,
             true
         )
-        headerBinding.tvNoteCardviewTitle.text = resources.getString(R.string.reminder_cardview_title)
-        headerBinding.spNoteCardviewCategory.also {
-            // TODO dummy content
-            it.adapter = CategorySpinnerAdapter(context = context, itemList = listOf("Generell", "Backlog", "Kp"))
-            it.dropDownHorizontalOffset = it.height
-        }
+        headerBinding.tvReminderCardviewTitle.text = resources.getString(R.string.reminder_cardview_title)
 
         // manage content
         contentBinding = DataBindingUtil.inflate(
