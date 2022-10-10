4eeb76495017e74853542d2fe0df637dabd47788
Jana-Ja <janajansen@live.de>
2022-10-10 02:32:54 +0200

    add reminder cardview

---
+++ app/src/main/java/janaja/organizer/adapter/ReminderRecyclerViewAdapter.kt
@@ -0,0 +1,64 @@
+package janaja.organizer.adapter
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import janaja.organizer.R
+import janaja.organizer.data.model.Note
+import janaja.organizer.ui.home.HomeFragmentDirections
+
+class ReminderRecyclerViewAdapter : RecyclerView.Adapter<ReminderRecyclerViewAdapter.ItemViewHolder>() {
+
+    private var dataset = mutableListOf<Note>()
+
+    class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val title: TextView = view.findViewById(R.id.note_title)
+        val body: TextView = view.findViewById(R.id.note_body)
+        val card: CardView = view.findViewById(R.id.note_card)
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun submitList(noteList: MutableList<Note>){
+        dataset = noteList
+        notifyDataSetChanged()
+    }
+
+    fun addItem(note: Note){
+        dataset.add(0, note)
+        notifyItemInserted(0)
+    }
+
+    fun removeitem(position: Int){
+        dataset.removeAt(position)
+        notifyItemRemoved(position)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val itemLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.reminder_list_item, parent, false)
+        return ItemViewHolder(itemLayout)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val note = dataset[position]
+        holder.title.text = note.title
+        holder.body.text = note.body
+        holder.card.setOnClickListener{
+            val navController = holder.itemView.findNavController()
+
+            navController.navigate(HomeFragmentDirections.actionHomeFragmentToNoteDetailFragment(note.id))
+
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return dataset.size
+    }
+
+
+}
--- app/src/main/java/janaja/organizer/ui/home/ReminderCardView.kt
+++ app/src/main/java/janaja/organizer/ui/home/ReminderCardView.kt
@@ -3,24 +3,22 @@ package janaja.organizer.ui.home
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import android.widget.Spinner
 import androidx.cardview.widget.CardView
 import androidx.databinding.DataBindingUtil
 import janaja.organizer.R
 import janaja.organizer.adapter.CategorySpinnerAdapter
-import janaja.organizer.adapter.NoteRecyclerViewAdapter
+import janaja.organizer.adapter.ReminderRecyclerViewAdapter
 import janaja.organizer.data.Repository
-import janaja.organizer.data.model.Note
 import janaja.organizer.databinding.HomeCardviewBinding
-import janaja.organizer.databinding.NoteCardviewContentBinding
 import janaja.organizer.databinding.NoteCardviewHeaderBinding
+import janaja.organizer.databinding.ReminderCardviewContentBinding
 
-class NoteCardView(context: Context, attrs: AttributeSet) : CardView(context, attrs) {
+class ReminderCardView(context: Context, attrs: AttributeSet) : CardView(context, attrs) {
 
     private val gurki = "beste babe"
     private val binding: HomeCardviewBinding
     private val headerBinding: NoteCardviewHeaderBinding
-    private val contentBinding: NoteCardviewContentBinding
+    private val contentBinding: ReminderCardviewContentBinding
 
     init {
         binding = DataBindingUtil.inflate(
@@ -31,33 +29,27 @@ class NoteCardView(context: Context, attrs: AttributeSet) : CardView(context, at
         )
 
         // manage header
-        headerBinding = DataBindingUtil.inflate<NoteCardviewHeaderBinding?>(
+        headerBinding = DataBindingUtil.inflate(
             LayoutInflater.from(context),
             R.layout.note_cardview_header,
             binding.flHomeCardviewHeader,
             true
         )
-        headerBinding.tvNoteCardviewTitle.text = resources.getString(R.string.note_cardview_title)
+        headerBinding.tvNoteCardviewTitle.text = resources.getString(R.string.reminder_cardview_title)
         headerBinding.spNoteCardviewCategory.also {
             // TODO dummy content
-            it.adapter = CategorySpinnerAdapter(context = context, itemList = listOf("Wichtig", "Kaufen", "Traumtagebuch", "Erinnerungen"))
+            it.adapter = CategorySpinnerAdapter(context = context, itemList = listOf("Generell", "Backlog", "Kp"))
             it.dropDownHorizontalOffset = it.height
         }
 
         // manage content
         contentBinding = DataBindingUtil.inflate(
             LayoutInflater.from(context),
-            R.layout.note_cardview_content,
+            R.layout.reminder_cardview_content,
             binding.flHomeCardviewContent,
             true
         )
-//        contentBinding.rvHomeCardviewNotes.addItemDecoration(
-//            DividerItemDecoration(
-//                context,
-//                LinearLayoutManager.VERTICAL
-//            )
-//        )
-        NoteRecyclerViewAdapter().also {
+        ReminderRecyclerViewAdapter().also {
             it.submitList(Repository.getInstance().dummyData)
             contentBinding.rvHomeCardviewNotes.adapter = it
         }
