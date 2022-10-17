69a82e28c1eb9d05e8e75d530238491d689d22c0
JanaJa <janajansen@live.de>
2022-10-17 21:28:45 +0200

    implement adding new line to checklist on enter

--- app/src/main/java/janaja/organizer/adapter/DetailNoteEntryRecyclerViewAdapter.kt
+++ app/src/main/java/janaja/organizer/adapter/DetailNoteEntryRecyclerViewAdapter.kt
@@ -11,24 +11,27 @@ import androidx.recyclerview.widget.RecyclerView
 import janaja.organizer.R
 import janaja.organizer.data.model.Line
 
-class DetailNoteEntryRecyclerViewAdapter(var dataset: MutableList<Line>) : RecyclerView.Adapter<DetailNoteEntryRecyclerViewAdapter.ItemViewHolder>(){
+class DetailNoteEntryRecyclerViewAdapter(var dataset: MutableList<Line>) :
+    RecyclerView.Adapter<DetailNoteEntryRecyclerViewAdapter.ItemViewHolder>() {
+
+    private lateinit var recyclerView: RecyclerView
 
     class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val lineText: EditText = view.findViewById(R.id.detail_note_entry_line)
         val checkBox: CheckBox = view.findViewById(R.id.detail_note_entry_checkBox)
     }
 
-    fun newLine(){
+    fun newLine() {
         dataset.add(Line("", false))
         notifyItemInserted(dataset.lastIndex)
     }
 
-    fun addItem(line: String){
-        dataset.add(Line(line, false))
-        notifyItemInserted(dataset.lastIndex)
+    fun addLine(position: Int, line: String) {
+        dataset.add(position, Line(line, false))
+        notifyItemInserted(position)
     }
 
-    fun removeitem(position: Int){
+    fun removeLine(position: Int) {
         dataset.removeAt(position)
         notifyItemRemoved(position)
     }
@@ -43,29 +46,50 @@ class DetailNoteEntryRecyclerViewAdapter(var dataset: MutableList<Line>) : Recyc
         val line = dataset[position]
         holder.lineText.setText(line.text)
         holder.checkBox.isChecked = line.isChecked
-        if(line.isChecked){
+        if (line.isChecked) {
             holder.lineText.paintFlags = holder.lineText.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
         } else {
-            holder.lineText.paintFlags = holder.lineText.paintFlags and Paint.STRIKE_THRU_TEXT_FLAG.inv()
+            holder.lineText.paintFlags =
+                holder.lineText.paintFlags and Paint.STRIKE_THRU_TEXT_FLAG.inv()
         }
 
         holder.checkBox.setOnClickListener {
             line.isChecked = holder.checkBox.isChecked
-            if(holder.checkBox.isChecked){
-                holder.lineText.paintFlags = holder.lineText.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+            if (holder.checkBox.isChecked) {
+                holder.lineText.paintFlags =
+                    holder.lineText.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
             } else {
-                holder.lineText.paintFlags = holder.lineText.paintFlags and Paint.STRIKE_THRU_TEXT_FLAG.inv()
+                holder.lineText.paintFlags =
+                    holder.lineText.paintFlags and Paint.STRIKE_THRU_TEXT_FLAG.inv()
             }
         }
 
-        holder.lineText.doAfterTextChanged { line.text = it.toString() }
+        holder.lineText.doAfterTextChanged {
+            var newText = it.toString()
+            if (it.toString().contains("\n")){
+                // finish this line
+                newText = newText.replace("\n","")
+                holder.lineText.setText(newText)
+                // add new Line
+                addLine(holder.layoutPosition + 1, "")
+            } else {
+                line.text = newText
+            }
+        }
+
+        holder.itemView.requestFocus()
     }
 
     override fun getItemCount(): Int {
         return dataset.size
     }
 
-    fun getAllLines(): MutableList<Line>{
+    fun getAllLines(): MutableList<Line> {
         return dataset
+    }
+
+    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
+        super.onAttachedToRecyclerView(recyclerView)
+        this.recyclerView = recyclerView
     }
 }
