a277983c88e75d7ff04ed5e7d9436d1d43c58cc5
Jana-Ja <janajansen@live.de>
2022-12-10 23:08:14 +0100

    remove unused value

--- app/src/main/java/janaja/organizer/data/model/Line.kt
+++ app/src/main/java/janaja/organizer/data/model/Line.kt
@@ -4,20 +4,19 @@ class Line(
     var id: Long,
     var text: String,
     var isChecked: Boolean = false,
-    var repeat: Boolean = false,
-    var notify: Boolean = false // TODO this is shit
+    var repeat: Boolean = false
 ) {
     override fun toString(): String {
         return text
     }
 
     fun equalContent(line: Line): Boolean {
-        if(notify != line.notify || repeat != line.repeat || text != line.text || isChecked != line.isChecked)
+        if(repeat != line.repeat || text != line.text || isChecked != line.isChecked)
             return false
         return true
     }
 
     fun copyLine(): Line {
-        return Line(id,text,isChecked,repeat,notify)
+        return Line(id,text,isChecked,repeat)
     }
 }
